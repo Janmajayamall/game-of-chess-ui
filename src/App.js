181581b321e5199636aeedc74dd64eb0e1d6a812
Janamajaya Mall <janmajaya.choithram@gmail.com>
2021-12-26 16:21:53 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Chessboard } from "react-chessboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Chessboard
+				arePiecesDraggable={false}
+				position={
+					"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0"
+				}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createClient, Provider as URQLProvider } from "urql";
+import { BrowserRouter as Router } from "react-router-dom";
+import { DAppProvider } from "@usedapp/core/packages/core";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const client = createClient({
+	url: "https://api.thegraph.com/subgraphs/name/janmajayamall/game-of-chess-subgraph",
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<URQLProvider value={client}>
+			{/* <Provider store={store}> */}
+			<DAppProvider
+				config={{
+					supportedChains: [421611],
+					// multicallAddresses: {
+					// 	421611: "0xed53fa304E7fcbab4E8aCB184F5FC6F69Ed54fF6",
+					// },
+					readOnlyUrls: {
+						421611: "https://rinkeby.arbitrum.io/rpc",
+					},
+				}}
+			>
+				<ChakraProvider>
+					<Router>
+						<App />
+					</Router>
+				</ChakraProvider>
+			</DAppProvider>
+			{/* </Provider> */}
+		</URQLProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
